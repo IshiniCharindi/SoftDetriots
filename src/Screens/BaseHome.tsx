6089fb84b2997cee0f3aef4a25749b5ffcfd9b43
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-04-13 03:16:54 +0530

    developing Home Page

--- src/Screens/BaseHome.tsx
+++ src/Screens/BaseHome.tsx
@@ -2,11 +2,18 @@ import Slider from "../Components/Slider.tsx";
 import ContactForm from "../Components/ContactForm.tsx";
 import {FeaturesSection} from "../Components/FeaturesSection.tsx";
 import Footer from "../Components/Footer.tsx";
+import NavBar from "../Components/NavBar.tsx";
+import Service from "../Components/Service.tsx";
+import Banner1 from "../assets/images/blog1.jpg";
+import BannerDetails from "../Components/BannerDetails.tsx";
 
 const BaseHome = () => {
     return (
         <div>
+            <NavBar/>
             <Slider/>
+            <Service/>
+            <BannerDetails reverse={true} img={Banner1} />
             <FeaturesSection/>
             <ContactForm/>
             <Footer/>
